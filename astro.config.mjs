831f9387e415ecdf1b171c8a8d684cf3f2561eb3
Rhub-r404 <rense.vandewalle@gmail.com>
2026-03-24 19:46:43 +0100

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://grondlaag.github.io',
+	base: '/grondlaag',
+	output: 'static',
+});
